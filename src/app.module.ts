db23df59c38d71bb3ad490557174259833361afc
atemcozz <atemcozzartem@gmail.com>
2024-01-31 22:33:44 +0300

    ssl fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ import * as path from "path";
           user: process.env["DATABASE_USER"],
           password: process.env["DATABASE_PASSWORD"],
           database: process.env["DATABASE_NAME"],
+          ssl: true, 
         },
       },
     }),
